f8d2c4df03163a43affaca1de136b9f973f6c73f
Luis Gustavo <luis.gustavo.freitas.a@gmail.com>
2024-08-07 19:20:34 -0300

    auto commit

---
+++ dio/tipos-e-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,16 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 1412.0;
+
+        short numeroCurto = 1;
+        int numeroLongo = numeroCurto;
+        short numeroCurto2 = (short) numeroLongo;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+    }
+}
