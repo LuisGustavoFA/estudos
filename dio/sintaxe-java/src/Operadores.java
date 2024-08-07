041ab10022d6913c830f58e9d3467c5f473b6fb7
Luis Gustavo <luis.gustavo.freitas.a@gmail.com>
2024-08-07 19:46:59 -0300

    auto commit

--- dio/sintaxe-java/src/Operadores.java
+++ dio/sintaxe-java/src/Operadores.java
@@ -16,5 +16,20 @@ public class Operadores {
 
         concatenacao = "1"+(1+1+1);
         System.out.println(concatenacao);
+
+        int numero = 1;
+        numero++;
+        numero = numero + 1;
+        numero += 1;
+        numero--;
+
+        System.out.println(numero);
+
+        int a = 3;
+        int b = 5;
+
+        String resultado = a==b ? "iguais" : "diferentes";
+
+        System.out.println(resultado);
     }
 }
