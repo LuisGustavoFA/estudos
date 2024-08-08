230eac204099e05febc8ecbfa61610ea8e174f98
Luis Gustavo <luis.gustavo.freitas.a@gmail.com>
2024-08-07 21:19:07 -0300

    auto commit

---
+++ dio/sintaxe-java/src/Javadoc.java
@@ -0,0 +1,33 @@
+/**
+* Classe Javadoc
+* @author Jonas
+* @version 1.0
+* @since 07/08/2024
+*/
+public class Javadoc {
+    public static void main(String[] args) {
+        //comentário de uma linha
+
+        /*Comentário 
+        de 
+        várias 
+        linhas*/
+
+        /**
+         * Comentário 
+         * de 
+         * documentação 
+         * Javadoc 
+        */
+    }
+
+    /**
+     * Método somar
+     * @param a Primeiro parâmetro para soma
+     * @param b Segundo parâmetro para soma
+     * @return Retorna o resultado da soma dos dois números
+     */
+    public int somar (int a, int b) {
+        return a+b;
+    }
+}
